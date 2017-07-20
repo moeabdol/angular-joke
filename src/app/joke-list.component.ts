b642ac8f81e95a98cfb17b721e0ccb44d234f015
moeabdol <mohd.a.saed@gmail.com>
2017-07-20 17:33:44 +0300

    Lif-cycle hooks

--- src/app/joke-list.component.ts
+++ src/app/joke-list.component.ts
@@ -7,24 +7,34 @@ import { Joke } from "./joke";
   templateUrl: "./joke-list.component.html"
 })
 export class JokeListComponent {
-  jokes: Joke[];
+  jokes: Joke[] = [];
 
-  constructor() {
-    this.jokes = [
-      new Joke("What did the cheese say when it looked in the mirror?",
-        "Hello-me (Halloumi)"),
-      new Joke("What kind of cheese do you use to disguise a small horse?",
-        "Mask-a-pony (Mascarpone)"),
-      new Joke("A kid threw a lump of cheddar at me",
-        "I thought 'That's not very mature'")
-    ];
+  // constructor() {
+  //   this.jokes = [
+  //     new Joke("What did the cheese say when it looked in the mirror?",
+  //       "Hello-me (Halloumi)"),
+  //     new Joke("What kind of cheese do you use to disguise a small horse?",
+  //       "Mask-a-pony (Mascarpone)"),
+  //     new Joke("A kid threw a lump of cheddar at me",
+  //       "I thought 'That's not very mature'")
+  //   ];
+  // }
+  //
+  // toggle(joke) {
+  //   joke.hide = !joke.hide;
+  // }
+  //
+  // addJoke(joke) {
+  //   this.jokes.unshift(joke);
+  // }
+
+  addJoke() {
+    this.jokes.unshift(
+      new Joke("What did the cheese say when it looked in the mirror",
+        "Hello-me (Halloumi)"));
   }
 
-  toggle(joke) {
-    joke.hide = !joke.hide;
-  }
-
-  addJoke(joke) {
-    this.jokes.unshift(joke);
+  deleteJoke() {
+    this.jokes = [];
   }
 }
--- src/app/joke.component.ts
+++ src/app/joke.component.ts
@@ -1,4 +1,14 @@
-import { Component, Input } from "@angular/core";
+import { Component,
+         Input,
+         OnChanges,
+         OnInit,
+         DoCheck,
+         AfterContentInit,
+         AfterContentChecked,
+         AfterViewInit,
+         AfterViewChecked,
+         OnDestroy,
+         SimpleChanges } from "@angular/core";
 
 import { Joke } from "./joke";
 
@@ -6,6 +16,57 @@ import { Joke } from "./joke";
   selector: "joke",
   templateUrl: "./joke.component.html"
 })
-export class JokeComponent {
+export class JokeComponent implements OnChanges,
+                                      OnInit,
+                                      DoCheck,
+                                      AfterContentInit,
+                                      AfterContentChecked,
+                                      AfterViewInit,
+                                      AfterViewChecked,
+                                      OnDestroy {
   @Input("joke") data: Joke;
+
+  constructor() {
+    console.log(`new - data is ${this.data}`);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(`ngOnChanges - data is ${this.data}`);
+    for (const key in changes) {
+      if (true) {
+        console.log(`${key} changed.
+            Current: ${changes[key].currentValue}.
+            Previous: ${changes[key].previousValue}.
+          `);
+      }
+    }
+  }
+
+  ngOnInit() {
+    console.log(`ngOnInit - data is ${this.data}`);
+  }
+
+  ngDoCheck() {
+    console.log("ngDoCheck");
+  }
+
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit");
+  }
+
+  ngAfterContentChecked() {
+    console.log("ngAfterContentChecked");
+  }
+
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit");
+  }
+
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked");
+  }
+
+  ngOnDestroy() {
+    console.log("ngOnDestroy");
+  }
 }
