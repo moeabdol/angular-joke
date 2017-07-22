56c84d7a5bda7de22edc3912eb309fc8b5cd6eb9
moeabdol <mohd.a.saed@gmail.com>
2017-07-22 18:31:46 +0300

    Directive input and configuration

--- src/app/card-hover.directive.ts
+++ src/app/card-hover.directive.ts
@@ -2,24 +2,30 @@ import { Directive,
          ElementRef,
          Renderer,
          HostListener,
-         HostBinding } from "@angular/core";
+         HostBinding,
+         Input } from "@angular/core";
 
 @Directive({
   selector: "[ccCardHover]"
 })
 export class CardHoverDirective {
   @HostBinding("class.card-outline-primary") private isHovering: boolean;
+  @Input("ccCardHover") config: Object = {
+    querySelector: ".card-text"
+  };
 
   constructor(private el: ElementRef, private renderer: Renderer) { }
 
   @HostListener("mouseover") onMouseOver() {
-    const part = this.el.nativeElement.querySelector(".card-text");
+    const part = this.el.nativeElement.querySelector(
+      this.config["querySelector"]);
     this.renderer.setElementStyle(part, "display", "block");
     this.isHovering = true;
   }
 
   @HostListener("mouseout") onMouseOut() {
-    const part = this.el.nativeElement.querySelector(".card-text");
+    const part = this.el.nativeElement.querySelector(
+      this.config["querySelector"]);
     this.renderer.setElementStyle(part, "display", "none");
     this.isHovering = false;
   }
